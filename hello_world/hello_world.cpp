4bf7b53fe2170ab98afae08cb6d40dd12bb43055
Ryan <kliu56@jhu.edu>
2021-07-08 14:48:58 +0800

    hello_world

---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //include all of the content of the library
+
+// declaration
+void Log(const char *);
+
+int main() // entry point of the project, so every project can just have one main function
+{
+    Log("Hello world");
+    std::cin.get();
+}
